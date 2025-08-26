05037ef44e3afe425c1509a62c4f9a2643be1e3e
zc <2535494488@qq.com>
2025-08-26 10:30:59 +0800

    通过webpack自定义标题名字

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
+const name =  'vue 111Admin Template' // page title
 // https://vite.dev/config/
 export default defineConfig({
-  lintOnSave: false,
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+      entry: '/src/main.js',
+      template: 'index.html',
+      inject: {
+        data: {
+          title: name
+        }
+      }
+    })
+  ],
 })
+
